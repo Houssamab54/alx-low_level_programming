af5039869a9e75ef486379225e6102f0b2d3fe8e
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-02-21 07:22:34 -0800

    ggg

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,56 +1,92 @@
 #include "main.h"
 #include <stdio.h>
 
-void print_number(int n)
-{
-    if (n < 0)
-    {
-        _putchar('-');
-        n *= -1;
-    }
-    if (n < 10)
-    {
-        _putchar(n + '0');
-    }
-    else if (n < 100)
-    {
-        _putchar(n / 10 + '0');
-        _putchar(n % 10 + '0');
-    }
-    else
-    {
-        _putchar(n / 100 + '0');
-        _putchar(n % 100 / 10 + '0');
-        _putchar(n % 10 + '0');
-    }
-}
-
-void print_comma_space(void)
-{
-    _putchar(',');
-    _putchar(' ');
-}
-
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: starting number
+ *
+ * Return: void
+ */
 void print_to_98(int n)
 {
-    int i;
+if (n <= 98)
+{
+print_up_to_98(n);
+}
+else
+{
+print_down_to_98(n);
+}
+}
 
-    if (n <= 98)
-    {
-        for (i = n; i < 98; i++)
-        {
-            print_number(i);
-            print_comma_space();
-        }
-    }
-    else
-    {
-        for (i = n; i > 98; i--)
-        {
-            print_number(i);
-            print_comma_space();
-        }
-    }
-    print_number(98);
-    _putchar('\n');
+/**
+ * print_up_to_98 - prints all natural numbers from n to 98, increasing
+ * @n: starting number
+ *
+ * Return: void
+ */
+void print_up_to_98(int n)
+{
+int j, i;
+for (i = n; i <= 98; i++)
+{
+j = i;
+if (i < 0)
+{
+_putchar('-');
+i *= -1;
+}
+if (i < 10)
+{
+_putchar(i + '0');
+}
+else if (i < 100)
+{
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
+}
+else
+{
+_putchar(i / 100 + '0');
+_putchar(i % 100 / 10 + '0');
+_putchar(i % 10 + '0');
+}
+if (j < 98)
+{
+_putchar(',');
+_putchar(' ');
+}
+i = j;
+_putchar('\n');
+}
+
+/**
+ * print_down_to_98 - prints all natural numbers from n to 98, decreasing
+ * @n: starting number
+ *
+ * Return: void
+ */
+void print_down_to_98(int n)
+{
+int i;
+for (i = n; i >= 98; i--)
+{
+if (i < 100)
+{
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
+}
+else
+{
+_putchar(i / 100 + '0');
+_putchar(i % 100 / 10 + '0');
+_putchar(i % 10 + '0');
+}
+if (i > 98)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
 }
