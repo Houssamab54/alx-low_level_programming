36d6ee3c274fe2195671a9e9e5f9e31e0deb987f
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-02-20 23:55:28 -0800

    ggg

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**                                                                                                                                                                            
+ * main - Entry point                                                                                                                                                          
+ *                                                                                                                                                                             
+ * Description: This function prints "_putchar" followed by a new line.                                                                                                        
+ *                                                                                                                                                                             
+ * Return: Always 0 (Success)                                                                                                                                                  
+ */
+
+int _abs(int n)
+{
+if (n < 0)
+{
+return (-n);
+}
+else
+{
+return (n);
+}
+}
