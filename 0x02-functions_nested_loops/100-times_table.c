bc3ef8eb2ce305aba39975c5a7fdf84bd107ead1
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-02-21 07:41:10 -0800

    ggg

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,51 +1,70 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
  * print_times_table - prints the n times table, starting with 0
- * @n: the table to be printed
+ * @n: the times table to print
  */
 void print_times_table(int n)
 {
-	int i, j, res;
-
 	if (n < 0 || n > 15)
 		return;
 
-	for (i = 0; i <= n; i++)
+	for (int i = 0; i <= n; i++)
 	{
-		for (j = 0; j <= n; j++)
-		{
-			res = i * j;
-
-			if (j != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-
-			if (res < 10 && j != 0)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(res + '0');
-			}
-			else if (res >= 10 && res < 100)
-			{
-				_putchar(' ');
-				_putchar(res / 10 + '0');
-				_putchar(res % 10 + '0');
-			}
-			else if (res >= 100 && j != 0)
-			{
-				_putchar(res / 100 + '0');
-				_putchar(res % 100 / 10 + '0');
-				_putchar(res % 10 + '0');
-			}
-			else
-				_putchar(res + '0');
-		}
+		for (int j = 0; j <= n; j++)
+			print_times_table_cell(i, j, n);
 
 		_putchar('\n');
+	}
+}
+
+/**
+ * print_times_table_cell - prints a cell in the times table
+ * @i: the row index
+ * @j: the column index
+ * @n: the size of the times table
+ */
+void print_times_table_cell(int i, int j, int n)
+{
+	int product = i * j;
+
+	if (j == 0)
+	{
+		print_number(product);
+	}
+	else
+	{
+		_putchar(' ');
+		print_number(product);
+	}
+
+	if (j < n)
+		_putchar(',');
+}
+
+/**
+ * print_number - prints a number
+ * @n: the number to print
+ */
+void print_number(int n)
+{
+	if (n >= 100)
+	{
+		_putchar(n / 100 + '0');
+		_putchar((n / 10) % 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else if (n >= 10)
+	{
+		_putchar(' ');
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else
+	{
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(n + '0');
 	}
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -40,4 +40,10 @@ void print_up_to_98(int n);
 
 void print_to_98(int n);
 
+void print_times_table(int n);
+
+void print_times_table_cell(int i, int j, int n);
+
+void print_number(int n);
+
 #endif /* main_H */
