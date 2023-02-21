0e177d8111824f28903cb987d5844d1370558e72
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-02-21 06:43:47 -0800

    ggg

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,68 +1,42 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
+
 /**
- * print_to_98 - Makes the sum of two numbers
- * @n: First operand
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: starting number
  *
- * Return: The sum of the two parameters
+ * Return: void
  */
 void print_to_98(int n)
 {
-int j, i;
-if (n < 98)
+int i;
+for (i = n; i <= 98; i++)
 {
-for (j = n ; j <= 98; j++)
+if (i < 0)
 {
-if (j < 10)
+putchar('-');
+i *= -1;
+}
+if (i < 10)
 {
-if (j < 0)
-{
-i = - j;
-_putchar('-');
 _putchar(i + '0');
 }
+else if (i < 100)
+{
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
+}
 else
 {
-_putchar(j + '0');
+_putchar(i / 100 + '0');
+_putchar(i % 100 / 10 + '0');
+_putchar(i % 10 + '0');
 }
-}
-else
-{
-_putchar(j / 10 + '0');
-_putchar(j % 10 + '0');
-}
-if (j != 98)
+if (i < 98)
 {
 _putchar(',');
 _putchar(' ');
 }
-else
-{
+}
 _putchar('\n');
-}
-}
-}
-if (n > 98)
-{
-for (j = n ; j >= 98; j--)
-{
-_putchar(j / 10 + '0');
-_putchar(j % 10 + '0');
-if (j != 98)
-{
-_putchar(',');
-_putchar(' ');
-}
-else
-{
-_putchar('\n');
-}
-}
-}
-if (n == 98)
-{
-_putchar(j / 10 + '0');
-_putchar(j % 10 + '0');
-_putchar('\n');
-}
 }
