c03e5ffcec7de918d4c4f724b170c5c0492a4b5f
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-02-21 02:10:05 -0800

    ggg

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include "main.h"
-
-/**                                                                                                                                                                            
- * main - Entry point                                                                                                                                                          
- *                                                                                                                                                                             
- * Description: This function prints "_putchar" followed by a new line.                                                                                                        
- *                                                                                                                                                                             
- * Return: Always 0 (Success)                                                                                                                                                  
+/**
+ * _isalpha - Makes the sum of two numbers
+ * @c: First operand
+ *
+ * Return: The sum of the two parameters
  */
-
 int _isalpha(int c)
 {
 if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
