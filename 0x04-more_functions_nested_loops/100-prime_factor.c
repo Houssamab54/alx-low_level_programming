31eaed84327d8af73b168b90d4c14d472713aead
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-02-23 11:15:29 -0800

    ggg

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,25 +8,22 @@
  */
 int main(void)
 {
-    unsigned long int n, div;
-
-    n = 612852475143;
-    div = 2;
-
-    while (n != 0)
-    {
-        if (n % div != 0)
-            div++;
-        else
-        {
-            n /= div;
-            if (n == 1)
-            {
-                printf("%lu\n", div);
-                break;
-            }
-        }
-    }
-
-    return (0);
+unsigned long int n, div;
+n = 612852475143;
+div = 2;
+while (n != 0)
+{
+if (n % div != 0)
+div++;
+else
+{
+n /= div;
+if (n == 1)
+{
+printf("%lu\n", div);
+break;
+}
+}
+}
+return (0);
 }
