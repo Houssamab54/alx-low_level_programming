da0b6925ac318e0bf4c3bbd5a225b3939a9b3c41
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-02-23 10:45:54 -0800

    ggg

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,18 +8,5 @@
  */
 void print_line(int n)
 {
-    if (n <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
-
-    int i;
-
-    for (i = 0; i < n; i++)
-    {
-        _putchar('_');
-    }
-
-    _putchar('\n');
+ 
 }
