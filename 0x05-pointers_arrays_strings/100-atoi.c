2b2d2e60bc7bec385ea6f5a6bcd08a96aecba3f3
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-02-27 02:43:41 -0800

    ggg

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,26 +10,21 @@
 int _atoi(char *s)
 {
     int sign = 1, result = 0;
-
-    while (*s)
-    {
-        if (*s == '-')
-        {
-            sign = -1;
-        }
-
-        if (*s >= '0' && *s <= '9')
-        {
-            result = result * 10 + (*s - '0');
-        }
-
-        if (*s == ';')
-        {
-            break;
-        }
-
-        s++;
-    }
-
-    return (result * sign);
+while (*s)
+{
+if (*s == '-')
+{
+sign = sign * -1;
+}
+if (*s >= '0' && *s <= '9')
+{
+result = result * 10 + (*s - '0');
+}
+if (*s == ';')
+{
+break;
+}
+s++;
+}
+return (result * sign);
 }
