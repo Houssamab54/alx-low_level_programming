e4407a29c6ca8cd67a14c64b4c545fcd413bc192
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-02-27 04:00:45 -0800

    ggg

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,7 +9,7 @@
  */
 int _atoi(char *s)
 {
-    int sign = 1, result = 0;
+int sign = 1, result = 0, digit = 0;
 while (*s)
 {
 if (*s == '-')
@@ -18,7 +18,15 @@ sign = sign * -1;
 }
 if (*s >= '0' && *s <= '9')
 {
-result = result * 10 + (*s - '0');
+digit = (*s - '0');
+if (result * 10 + digit >= 2147483647)
+{
+return (2147483648 * sign);
+}
+else
+{
+result = result * 10 + digit;
+}
 }
 if (*s == ';')
 {
@@ -26,5 +34,5 @@ break;
 }
 s++;
 }
-return (result * sign);
+return (result *sign);
 }
