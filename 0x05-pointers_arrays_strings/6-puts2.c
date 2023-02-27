495b37fb981e1513baa2205cf2f17cb3cca7a5df
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-02-27 02:00:19 -0800

    ggg

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,5 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * puts2 - prints every other character of a string, starting with the first
@@ -9,11 +10,17 @@
  */
 void puts2(char *str)
 {
-int i = 0;
-while (*(str + i) != '\0')
-{
-_putchar(*(str + i));
-i += 2;
-}
-_putchar('\n');
+    int i = 0;
+
+    while (*(str + i) != '\0') {
+        putchar(*(str + i));
+        i += 2;
+    }
+
+    putchar('\n');
+
+    // deallocate memory if it was dynamically allocated
+    if (str != NULL) {
+        free(str);
+    }
 }
