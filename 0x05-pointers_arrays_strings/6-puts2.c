bd35f3741aadfb530d60c9b0e5cc8cb58e8389f3
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-02-27 02:10:20 -0800

    ggg

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,16 +11,10 @@
 void puts2(char *str)
 {
 int i = 0;
-while (*(str + i) != '\0')
+while (*(str + i) != '\0' && *(str + i-1) != '\0')
 {
 _putchar(*(str + i));
 i += 2;
 }
 _putchar('\n');
-i = 0;
-while (*(str + i) != '\0')
-{
-*(str + i) = '\0';
-i += 1;
-}
 }
