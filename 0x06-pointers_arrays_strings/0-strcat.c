c76c4cfb67ec6d78224415c32c38e868b902a336
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-03-01 07:17:45 -0800

    ggg

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,20 +11,17 @@
 
 char *_strcat(char *dest, char *src)
 {
-    char *start = dest;
-
-    while (*dest != '\0')
-    {
-        dest++;
-    }
-
-    while (*src != '\0')
-    {
-        *dest = *src;
-        dest++;
-        src++;
-    }
-
-    *dest = '\0';
-    return (start);
+char *start = dest;
+while (*dest != '\0')
+{
+dest++;
+}
+while (*src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+}
+*dest = '\0';
+return (start);
 }
