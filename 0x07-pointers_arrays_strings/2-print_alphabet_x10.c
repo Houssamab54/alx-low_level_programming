a44033074bcf8b6cebac89a7815546e036df8698
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-03-06 03:03:50 -0800

    ggg

---
+++ 0x07-pointers_arrays_strings/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Description: This function prints "_putchar" followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+int i, j;
+for (j = 0 ; j < 10; j++)
+{
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
+}
