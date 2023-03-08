61359332876cb1524c3633015327675759d2b814
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-03-08 03:24:01 -0800

    ggg

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -10,7 +10,7 @@
 
 int is_palindrome(char *s)
 {
-	int len = _strlen_recursion(s);
+	int len = _strlen(s);
 	return (check_palindrome(s, 0, len - 1));
 }
 
@@ -31,4 +31,22 @@ int check_palindrome(char *s, int start, int end)
 	if (s[start] != s[end])
 		return (0);
 	return (check_palindrome(s, start + 1, end - 1));
+}
+
+/**
+ * _strlen - Computes the length of a string.
+ *
+ * @s: The string to compute the length of.
+ *
+ * Return: The length of the string.
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s++)
+		len++;
+
+	return (len);
 }
