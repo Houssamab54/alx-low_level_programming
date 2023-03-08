d9a445e26a33f09e71c2ca5b066ca22111b93279
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-03-08 02:01:05 -0800

    ggg

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -28,6 +28,8 @@ int _sqrt_recursion(int n);
 
 int is_prime_number(int n);
 
+int calculate_sqrt(int n, int start, int end);
+
 void set_string(char **s, char *to);
 
 char *_memset(char *s, char b, unsigned int n);
