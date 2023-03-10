b7bc8838593f085676d890f9e4d9833e2c639e4d
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-03-10 01:31:20 -0800

    ggg

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ **/
+int main(int argc, char *argv[])
+{
+    (void)argc; /* unused parameter */
+
+    printf("%s\n", argv[0]);
+
+    return (EXIT_SUCCESS);
+}
