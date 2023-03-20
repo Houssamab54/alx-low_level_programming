43835ae90e55e46887fbc219508188f43a992aed
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-03-20 07:50:08 -0700

    ggg

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,7 @@
+/**
+ * ABS - Compute the absolute value of a number
+ * @x: The number to compute the absolute value of
+ *
+ * Return: The absolute value of x
+ */
+#define ABS(x) ((x) < 0 ? -(x) : (x))
