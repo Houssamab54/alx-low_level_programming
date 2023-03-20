00b0d02998a527258c4b8b8baab4f073fd9b059e
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-03-20 05:27:23 -0700

    ggg

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - a structure representing a dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ */
+typedef struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+} dog_t;
