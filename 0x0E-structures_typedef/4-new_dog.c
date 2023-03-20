05e0b79b8f8f983ac1876c6120d652eaf9b9d001
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-03-20 06:34:04 -0700

    ggg

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,33 +3,26 @@
 #include "main.h"
 #include "dog.h"
 
-/**                                                                                                                                                  
- * _strdup - returns a pointer to a newly allocated space in memory,                                                                                 
- * which contains a copy of the string given as a parameter.                                                                                         
- * @str: string to be duplicated                                                                                                                     
- *                                                                                                                                                   
- * Return: pointer to the du insufficient memory was available                                                                                       
+/**
+ * _strdup - returns a pointer located space in
+ * which contains a copy of theiven as a paramet
+ * @str: string to be d
+ * Return: pointer to the du insufficient memory
  */
 char *_strdup(char *str)
 {
-        char *duplicate;
-        int length = 0, i;
-
-        if (str == NULL)
-                return (NULL);
-
-        while (str[length])
-                length++;
-
-        duplicate = malloc(sizeof(char) * (length + 1));
-
-        if (duplicate == NULL)
-                return (NULL);
-
-        for (i = 0; i <= length; i++)
-                duplicate[i] = str[i];
-
-        return (duplicate);
+char *duplicate;
+int length = 0, i;
+if (str == NULL)
+return (NULL);
+while (str[length])
+length++;
+duplicate = malloc(sizeof(char) * (length + 1));
+if (duplicate == NULL)
+return (NULL);
+for (i = 0; i <= length; i++)
+duplicate[i] = str[i];
+return (duplicate);
 }
 
 /**
@@ -38,34 +31,30 @@ char *_strdup(char *str)
  * @age: age of the dog
  * @owner: owner of the dog
  *
- * Return: pointer to the new dog, or NULL if function fails
+ * Return: pointer to the new dunction fails
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *new_dog;
-
-    /* allocate memory for new dog */
-    new_dog = malloc(sizeof(dog_t));
-    if (new_dog == NULL)
-        return (NULL);
-
-    /* make copies of name and owner */
-    new_dog->name = _strdup(name);
-    if (new_dog->name == NULL)
-    {
-        free(new_dog);
-        return (NULL);
-    }
-    new_dog->owner = _strdup(owner);
-    if (new_dog->owner == NULL)
-    {
-        free(new_dog->name);
-        free(new_dog);
-        return (NULL);
-    }
-
-    /* set age of new dog */
-    new_dog->age = age;
-
-    return (new_dog);
+dog_t *new_dog;
+/* allocate memory for new dog */
+new_dog = malloc(sizeof(dog_t));
+if (new_dog == NULL)
+return (NULL);
+/* make copies of name and owner */
+new_dog->name = _strdup(name);
+if (new_dog->name == NULL)
+{
+free(new_dog);
+return (NULL);
+}
+new_dog->owner = _strdup(owner);
+if (new_dog->owner == NULL)
+{
+free(new_dog->name);
+free(new_dog);
+return (NULL);
+}
+/* set age of new dog */
+new_dog->age = age;
+return (new_dog);
 }
