97ec307d9999ffc8f989a280b12e311492b18e56
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-03-20 07:51:57 -0700

    ggg

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,12 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
 /**
- * ABS - Compute the absolute value of a number
- * @x: The number to compute the absolute value of
+ * ABS - Computes the absolute value of a number
+ * @x: The number whose absolute value is to be computed
  *
  * Return: The absolute value of x
  */
 #define ABS(x) ((x) < 0 ? -(x) : (x))
+
+#endif /* FUNCTION_LIKE_MACRO_H */
