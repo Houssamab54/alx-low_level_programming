a574e8e8949fe217b7c78cd7d8804e453004b33c
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-03-22 07:49:46 -0700

    ggg

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,55 +10,48 @@
  * @argc: Number of arguments
  * @argv: Array of arguments
  *
- * Return: 0 on success, 98 on wrong number of arguments, 99 on invalid operator,
- *         100 on division/modulo by 0.
+ * Return: 0 on success, 98 on wrong number of argument,
  */
 int main(int argc, char *argv[])
 {
-    int a, b, result;
-    char *operator;
-    int (*op_func)(int, int);
-
-    if (argc != 4)
-    {
-        _putchar('E');
-        _putchar('r');
-        _putchar('r');
-        _putchar('o');
-        _putchar('r');
-        _putchar('\n');
-        return (98);
-    }
-
-    a = atoi(argv[1]);
-    b = atoi(argv[3]);
-    operator = argv[2];
-
-    op_func = get_op_func(operator);
-
-    if (op_func == NULL)
-    {
-        _putchar('E');
-        _putchar('r');
-        _putchar('r');
-        _putchar('o');
-        _putchar('r');
-        _putchar('\n');
-        return (99);
-    }
-
-    if ((*operator == '/' || *operator == '%') && b == 0)
-    {
-        _putchar('E');
-        _putchar('r');
-        _putchar('r');
-        _putchar('o');
-        _putchar('r');
-        _putchar('\n');
-        return (100);
-    }
-
+int a, b, result;
+char *operator;
+int (*op_func)(int, int);
+if (argc != 4)
+{
+_putchar('E');
+_putchar('r');
+_putchar('r');
+_putchar('o');
+_putchar('r');
+_putchar('\n');
+return (98);
+}
+a = atoi(argv[1]);
+b = atoi(argv[3]);
+operator = argv[2];
+op_func = get_op_func(operator);
+if (op_func == NULL)
+{
+_putchar('E');
+_putchar('r');
+_putchar('r');
+_putchar('o');
+_putchar('r');
+_putchar('\n');
+return (99);
+}
+if ((*operator == '/' || *operator == '%') && b == 0)
+{
+_putchar('E');
+_putchar('r');
+_putchar('r');
+_putchar('o');
+_putchar('r');
+_putchar('\n');
+return (100);
+}
 result = op_func(a, b);
- printf("%d\n",result);
+printf("%d\n", result);
 return (0);
 }
