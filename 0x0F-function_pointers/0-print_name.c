ead846541618a85bb66d05ca871c825e01c1dd4e
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-03-22 03:19:11 -0700

    ggg

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - prints a name as is
+ * @name: name of the person
+ * @(*f)(char *): ggggfgf
+ *
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+(*f)(name);
+}
