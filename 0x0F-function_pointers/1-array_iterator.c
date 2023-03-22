8979db8bd33f3df0af822fa70b73c72e868263cf
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-03-22 03:59:21 -0700

    ggg

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,19 +9,14 @@
  *
  * Return: Nothing.
  */
-
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    int i;
-    int j = 0;
-    if(j != 0)
-    {
-    if (action != NULL && size != 0)
-    {
-    for (i = 0; i < (int) size; i++)
-    {
-        (*action)(*(array+i));
-    }
-    }
-    }
+int i;
+if (action != NULL && size != 0 && array != NULL)
+{
+for (i = 0; i < (int) size; i++)
+{
+(*action)(*(array+i));
+}
+}
 }
