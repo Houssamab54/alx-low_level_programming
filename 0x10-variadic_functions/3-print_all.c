6b54a7de74d8a01bab9402846a442e75cf4cfdb7
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-03-23 14:22:34 -0700

    ggg

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,7 +1,6 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
-
 /**
  * print_all - Prints anything.
  *
@@ -10,15 +9,52 @@
  *          i: integer
  *          f: float
  *          s: char * (if the string is NULL, print (nil) instead)
- *          Any other char should be ignored.
  *
  * Return: void.
  */
 void print_all(const char * const format, ...)
 {
-va_list args;
-va_start(args, format);
-printf("(nil)");
-printf("\n");
-va_end(args);
+	va_list args;
+	unsigned int i = 0;
+	char c;
+	char *s;
+	void *p;
+	int n;
+	float f;
+	va_start(args, format);
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				c = (char) va_arg(args, int);
+				printf("%c", c);
+				break;
+			case 'i':
+				n = va_arg(args, int);
+				printf("%d", n);
+				break;
+			case 'f':
+				f = (float) va_arg(args, double);
+				printf("%f", f);
+				break;
+			case 's':
+				s = va_arg(args, char *);
+				if (s == NULL)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", s);
+				break;
+			default:
+				i++;
+				continue;
+		}
+		if (format[i + 1])
+			printf(", ");
+		i++;
+	}
+	va_end(args);
+	printf("\n");
 }
