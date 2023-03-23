41c2a8d2ddbae937513699b7f918caaccc47ba39
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-03-23 07:39:05 -0700

    ggg

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -50,99 +50,9 @@ int wildcmp(char *s1, char *s2);
 
 int **alloc_grid(int width, int height);
 
-void free_grid(int **grid, int height);
-
-char *_strdup(char *str);
-
-int factorial(int n);
-
-int _pow_recursion(int x, int y);
-
-int _sqrt_recursion(int n);
-
-int is_palindrome(char *s);
-
-int check_palindrome(char *s, int start, int end);
-
-int is_prime_number(int n);
-
-int calculate_sqrt(int n, int start, int end);
-
-void set_string(char **s, char *to);
-
-int is_prime(int n, int i);
-
-char *_memset(char *s, char b, unsigned int n);
-
-char *_memcpy(char *dest, char *src, unsigned int n);
-
-char *_strchr(char *s, char c);
-
-unsigned int _strspn(char *s, char *accept);
-
-char *_strpbrk(char *s, char *accept);
-
-char *_strstr(char *haystack, char *needle);
-
-void print_chessboard(char (*a)[8]);
-
-void print_diagsums(int *a, int size);
-
-int _strlen(char *s);
-
-void _puts(char *str);
-
-void print_rev(char *s);
-
-void rev_string(char *s);
-
-void puts2(char *str);
-
-void puts_half(char *str);
-
-void print_array(int *a, int n);
-
-char *_strcpy(char *dest, char *src);
-
-void swap_int(int *a, int *b);
-
-void print_alphabet(void);
-
-void print_alphabet_x10(void);
-
-int _islower(int c);
-
-int _isalpha(int c);
-
-int print_sign(int n);
-
-int _abs(int);
-
-int print_last_digit(int);
-
-void jack_bauer(void);
-
-void times_table(void);
-
-int add(int, int);
-
-void print_down_to_98(int n);
-
-void print_up_to_98(int n);
-
-void print_to_98(int n);
-
-void print_times_table(int n);
-
-void print_times_table_cell(int i, int j, int n);
-
-int _isupper(int c);
-
 int _isdigit(int c);
 
 int mul(int a, int b);
-
-void print_numbers(void);
 
 void print_most_numbers(void);
 
