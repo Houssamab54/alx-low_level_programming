e6af8308744d9bf787b3d33330db1a289db99e1c
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-03-30 03:11:01 -0700

    ggg

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,8 +1,26 @@
 #ifndef main_H
 #define main_H
 
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include <unistd.h>
 #include "dog.h"
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
 
 /**
  * _putchar - writes the character c to stdout
