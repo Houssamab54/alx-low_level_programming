a986db532b323e7fb7f5148f17d7956004df4d56
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-04-03 13:03:25 -0700

    ggg

---
+++ 0x13-more_singly_linked_lists/10-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - Makes the sum of two numbers
+ * @arg1: First operand
+ * @arg2: Second operand
+ *
+ * Return: The sum of the two parameters
+ */
+int add(int arg1, int arg2)
+{
+	return (arg1 + arg2);
+}
