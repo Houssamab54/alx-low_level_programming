90efaa36bea89d03240698d6da69cc765e46492b
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-04-09 04:32:51 -0700

    ggg

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,6 +4,39 @@
 #include <unistd.h>
 #include "dog.h"
 
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+char *str;
+unsigned int len;
+struct list_s *next;
+}
+list_t;
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
+typedef struct listint_s
+{
+int n;
+struct listint_s *next;
+}
+listint_t;
+
+
 /**
  * _putchar - writes the character c to stdout
  *
