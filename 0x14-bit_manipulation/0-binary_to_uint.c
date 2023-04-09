078270a66d5099e3aa5d3cd095cf4b9304b5d458
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-04-09 04:43:51 -0700

    ggg

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,26 +5,26 @@
  * @b: pointer to the binary number string
  *
  * Return: the converted number, or 0 if there is one or more chars in the
- * string b that is not 0 or 1, or if b is NULL.
+ *         string b that is not 0 or 1, or if b is NULL.
  */
 unsigned int binary_to_uint(const char *b)
 {
-        unsigned int result = 0;
-        int i;
+	unsigned int result = 0;
+	int i;
 
-        if (b == NULL)
-                return (0);
+	if (b == NULL)
+		return (0);
 
-        for (i = 0; b[i] != '\0'; i++)
-        {
-                if (b[i] == '0' || b[i] == '1')
-                {
-                        result <<= 1;
-                        result += b[i] - '0';
-                }
-                else
-                        return (0);
-        }
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] == '0' || b[i] == '1')
+		{
+			result <<= 1;
+			result += b[i] - '0';
+		}
+		else
+			return (0);
+	}
 
-        return (result);
+	return (result);
 }
