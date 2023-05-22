a439323a85b88f37358648fd857769101a98b973
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-05-22 03:22:34 -0700

    ggg

---
+++ 0x18-dynamic_libraries/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - prints an integer, in hexadecimal
+ * @array: the integer to print
+ * @size: the integer to print
+ * @action: the integer to print
+ *
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+int i;
+if (action != NULL && size != 0 && array != NULL)
+{
+for (i = 0; i < (int) size; i++)
+{
+(*action)(*(array + i));
+}
+}
+}
